8550fa61d38885a74c3e62ec0ade4ba2691d3982
Oscar Karlsson <osc16kar@gmail.com>
2023-06-26 22:35:03 +0200

    Comments and addeed find_set_stepsize()

--- game_engine/Timestep.py
+++ game_engine/Timestep.py
@@ -1,37 +1,71 @@
 import numpy as np
+import logging
 
 class TimeFuncs:
-        def __init__(self, t = 0,  h = 1/60):
-              self.t = t
-              self.h = h
+    def __init__(self, t:float = 0,  h:float = 0):
+        """
+        Instantiates a new TimeFuncs object with time t and stepsize h
 
-        def setTime(self, t):
-              self.t = t
+        Parameters
+        ----------
+        t : float
+            The current time in seconds
+        h : float
+            The default stepsize
+        """
+        self.t = t
+        self.h = h
 
-        def setStepSize(self, h):
-              self.h = h
+    def set_time(self, t:float):
+        """
+        Sets the current time
 
-        def timestep(self, f:np.matrix, u:np.matrix):
-            """
-            Takes a time step forward using the framework u_y+1 = u_y + du/dt_y+1
-            f is a matrix such that fu gives du\dt so u_y+1 = u_y
+        Parameters
+        ----------
+        t : float
+            Current time in seconds, reccomend using time.time or manually counting time from 0
+        """
+        self.t = t
+
+    def find_set_stepsize(self, t:float):
+        h = t - self.t
+        if h <= 0 : 
+            h = 0
+            logging.warning('Negative or zero stepsize found at time: ' + str(t))
+        self.h = h
+        return h
+
+    def setStepSize(self, h:float):
+        """
+        Manually sets the current stepsize to a value h
+
+        Parameters
+        ----------
+        h : float
+            The new stepsize
+        """
+        self.h = h
+
+    def timestep(self, f:np.matrix, u:np.matrix, h:float = None):
+        """
+        Takes a time step forward using the framework u_y+1 = u_y + du/dt_y+1
+        f is a matrix such that fu gives du\dt so u_y+1 = u_y
+        
+        Parameters
+        ----------
+        f : np.matrix or array that can be used as matrix
+            Derivative matrix
+        u : np.matrix or array that can be used as matrix
+            Initial state for some variable or collection of variables
             
-            Parameters
-            ----------
-            f : np.matrix or array that can be used as matrix
-                Derivative matrix
-            u : np.matrix or array that can be used as matrix
-                Initial state for some variable or collection of variables
-            
-            Returns
-            -------
-            u : np.matrix
-                Updated state
-            t : float
-                New time
-            """
-            f = np.mat(f)
-            u = np.mat(u)
-            unew = np.linalg.solve(np.eye(f.shape[0]) - self.h*f, u)
-            self.t = self.t + self.h
-            return [unew, self.t]
+        Returns
+        -------
+        u : np.matrix
+            Updated state
+        """
+        if h is None: h = self.h
+         
+        f = np.mat(f)
+        u = np.mat(u)
+        unew = np.linalg.solve(np.eye(f.shape[0]) - self.h*f, u)
+        return unew
